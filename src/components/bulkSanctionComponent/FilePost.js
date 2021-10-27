f23f4fcdf8239808ca2de0986d7b47649d16ee17
AMSL\maidul <mi_shawon@yahoo.com>
2021-10-27 17:24:18 +0600

    id type condition added

--- src/components/bulkSanctionComponent/FilePost.js
+++ src/components/bulkSanctionComponent/FilePost.js
@@ -22,7 +22,8 @@ const FilePost = ({ ExcelData }) => {
       getsanctionTypeID(x["Santion Type"]) > 0 &&
       getsanctionTypeID(x["Santion Type"]) < 7 &&
       getIdentificationTypeID(x["ID TYPE"]) > 0 &&
-      getIdentificationTypeID(x["ID TYPE"]) < 7
+      getIdentificationTypeID(x["ID TYPE"]) < 7 &&
+      x["ID NUMBER"] !== ""
         ? validDataset.push({
             SanctionPartyType: 1,
             Name: x.Surname || x.Institution,
@@ -42,20 +43,21 @@ const FilePost = ({ ExcelData }) => {
             SanctionExpiration: convertDate(x["SANCTION EXPIRATION"]),
           })
         : invalidDataset.push({
-            SanctionPartyType: 1,
-            Name: x.Surname || x.Institution,
-            OtherName: x["Other Name(s)"] || x.ADDRESS,
-            DateOfBirth: x["DATE OF BIRTH"]
+            Surname: x.Surname || x.Institution,
+            "Other Name(s)": x["Other Name(s)"] || x.ADDRESS,
+            "DATE OF BIRTH": x["DATE OF BIRTH"]
               ? convertDate(x["DATE OF BIRTH"])
               : null,
-            Nationality: x.NATIONALITY ? x.NATIONALITY : null,
-            Address: x["BUSINESS REG NUMBER"] ? x["BUSINESS REG NUMBER"] : "",
-            IdentificationType: x["ID TYPE"],
-            IdentificationNumber: x["ID NUMBER"] || null,
-            SanctionTypeId: x["Santion Type"] || x["Sanction Type"],
-            SanctionDetails: x["SANCTION DETAIL"] || x["SANCTION Details"],
-            SanctionDate: convertDate(x["SANCTION DATE"]),
-            SanctionExpiration: convertDate(x["SANCTION EXPIRATION"]),
+            NATIONALITY: x.NATIONALITY ? x.NATIONALITY : null,
+            "BUSINESS REG NUMBER": x["BUSINESS REG NUMBER"]
+              ? x["BUSINESS REG NUMBER"]
+              : "",
+            "ID TYPE": x["ID TYPE"],
+            "ID NUMBER": x["ID NUMBER"],
+            "Santion Type": x["Santion Type"] || x["Sanction Type"],
+            "SANCTION DETAIL": x["SANCTION DETAIL"] || x["SANCTION Details"],
+            "SANCTION DATE": convertDate(x["SANCTION DATE"]),
+            "SANCTION EXPIRATION": convertDate(x["SANCTION EXPIRATION"]),
           })
     );
     setState(validDataset);
@@ -90,7 +92,7 @@ const FilePost = ({ ExcelData }) => {
         Submit
       </Button>
       &nbsp;&nbsp;
-      {invalidState ? (
+      {invalidState.length > 0 ? (
         <Button
           type="button"
           variant="outline-info"
